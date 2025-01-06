be868ce1ff82b52b0583496e65fd26cb4b3de495
Deeh878 <dainesskato@gmail.com>
2025-01-06 22:23:28 +0300

    Add files via upload

--- polynomialSolverProject.cpp
+++ polynomialSolverProject.cpp
@@ -1,3 +1,9 @@
 #include <iostream>
 #include <map>
 using namespace std;
+
+// Function to add two polynomials
+map<int, int> addPolynomials(const map<int, int>& p1, const map<int, int>& p2) {
+    map<int, int> result = p1;
+    for (const auto& term : p2) {
+  
